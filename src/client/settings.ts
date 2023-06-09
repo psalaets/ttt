af4b2d3febcba42ec877a1701d83ff9aac2d1674
Paul Salaets <psalaets@gmail.com>
2023-06-09 12:59:42 -0500

    Read server url from env vars

--- src/client/settings.ts
+++ src/client/settings.ts
@@ -1,2 +1,7 @@
-export const GAME_SERVER_URL = 'http://localhost:8080';
+export const GAME_SERVER_URL = process.env.SERVER_URL;
+
+if (!GAME_SERVER_URL) {
+  throw new Error('Environment variable SERVER_URL must be set');
+}
+
 export const LOBBY_URL = GAME_SERVER_URL;

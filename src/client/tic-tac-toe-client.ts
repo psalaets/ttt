43439f2d3277576524f23eed5630b287e4fe04a4
Paul Salaets <psalaets@gmail.com>
2023-06-09 18:24:26 -0500

    Force ttt client to have typed moves object

--- src/client/tic-tac-toe-client.ts
+++ src/client/tic-tac-toe-client.ts
@@ -1,12 +1,16 @@
 import { Client } from 'boardgame.io/client';
 import { SocketIO } from 'boardgame.io/multiplayer'
-import { GameState, ticTacToe } from '../game/tic-tac-toe';
+import { ClientMoves, GameState, ticTacToe } from '../game/tic-tac-toe';
 import { GAME_SERVER_URL } from './urls';
 
-export type TicTacToeClient = ReturnType<typeof Client<GameState>>;
+type GenericClient<T extends any> = ReturnType<typeof Client<T>>;
+
+export type TicTacToeClient = GenericClient<GameState> & {
+  moves: ClientMoves,
+}
 export type ClientState = ReturnType<TicTacToeClient['getState']>;
 
-export function create(matchId: string, playerId: string, credentials: string): TicTacToeClient {
+export function create(matchId: string, playerId: string, credentials: string) {
   const c = Client({
     game: ticTacToe,
     numPlayers: 2,
@@ -18,5 +22,5 @@ export function create(matchId: string, playerId: string, credentials: string): 
     credentials,
   });
 
-  return c;
+  return c as TicTacToeClient;
 }

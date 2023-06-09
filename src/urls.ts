21360bf5ac8f0cb44ed65cd5d3079cfdf7a3e7a2
Paul Salaets <psalaets@gmail.com>
2023-06-09 17:26:32 -0500

    Need this prefix for env var to be included in UI bundle

--- src/urls.ts
+++ src/urls.ts
@@ -1,4 +1,4 @@
-export const GAME_SERVER_URL = process.env.SERVER_URL;
+export const GAME_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 if (!GAME_SERVER_URL) {
   throw new Error('Environment variable SERVER_URL must be set');
